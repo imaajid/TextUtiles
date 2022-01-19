596c6ef879067ee65649e4e7a986db62c3fa8fb4
imajidd <97963624+imajidd@users.noreply.github.com>
2022-01-19 20:13:34 +0500

    React js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+     <Navbar title="TextUtile" home="Home" link="Link" content="Content"/>
+    </>
+  );
+}
+
+export default App;
